667f0b2232096e3ef4b808679787b66051832939
Ele-rigal <riwgal2@gmail.com>
2019-01-03 16:56:47 +0100

    Add files via upload
    j'avais besoin de ces deux classes pour pouvoir commencer le jeu, à toi de voir pour les organiser et si tu en as besoin ou pas

---
+++ domino/affichage/Joueur.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.math.*;
+//package ProjoPOOIG.domino.jeu;
+//import domino.pieces.PieceD;
+//import ProjoPOOIG.domino.jeu;
+
+public class Joueur{
+	private String nom;
+	private ArrayList<Piece> main;
+	private boolean situation=true; //true si joue encore false si perdu
+	//private int nbPoints=0;
+	private int indice; //numéro de tour du joueur
+	public Joueur(String nom,int i,Sac s){
+		this.nom=nom;
+		indice=i;
+		main=new ArrayList<Piece>();
+		service(s);
+		//puis autres fonctions
+		//ajouterPoint();
+		//perdu();
+		//remplir ou vider main 
+	}
+	public ArrayList<Piece> service(Sac s){
+		int [] t=new int[7];
+		int n=-1;
+		PieceD p;
+		for(int i=0;i<7;i++){
+			n=(int)(Math.random()*s.getNbPieces());
+			t[i]=n;
+			System.out.println(t[i]);
+			p=s.distribuPiece(n);
+			main.add(p);
+			s.enleverPieceSac(n);
+		}
+		
+		return main;
+	}
+	public ArrayList<Piece> getPiece(){
+		return main;
+	}
+		
+}
+
---
+++ domino/affichage/Sac.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+//package ProjoPOOIG.domino.pieces;
+import ProjoPOOIG.domino.pieces.PieceD;
+//import ProjoPOOIG.domino.jeu;
+public class Sac{
+	private ArrayList<PieceD> sac;
+	private static int nbPieces=28;
+	public Sac(){
+		sac=new ArrayList<PieceD>();
+		remplirSac();
+	}
+	public void remplirSac(){
+		PieceD p;
+		//int ref=0;
+		for(int i=0;i<7;i++){
+			for(int j=i;j<7;j++){
+				p=new PieceD(i,j);
+				sac.add(p);
+				//ref++;
+				//System.out.println(ref+" "+i+"|"+j);
+			}
+		}
+	}
+	public int getNbPieces(){
+		return nbPieces;
+	}
+				
+	public void enleverPieceSac(int i){
+		nbPieces--;
+		sac.remove(i);
+	}
+	public PieceD distribuPiece(int i){
+		PieceD p=sac.get(i);
+		return p;
+	}
+	public void afficheSac(){
+		int ref=0;
+		for(int i=0;i<sac.size();i++){
+			ref++;
+			System.out.println(ref+" "+sac.get(i));
+		}
+	}
+}
